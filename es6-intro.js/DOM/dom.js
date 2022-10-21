25d53d10f9bc6b5a0e7e3db09d4aabd1ef25a730
erobinofficial <robiul8778@gmail.com>
2022-10-21 19:21:17 +0600

    dom added

--- es6-intro.js/DOM/dom.js
+++ es6-intro.js/DOM/dom.js
@@ -1,3 +1,20 @@
-function nameOn() {
-    document.getElementsByClassName("name")[0].style.display = "block";    
-}
+document.getElementById('add-border').addEventListener('click', function(){
+    const container = document.getElementById('friend-container');
+    container.style.border = '3px solid red';
+});
+document.getElementById('add-bg').addEventListener('click', function(){
+    const friends = document.getElementsByClassName('friend');
+        for(const friend of friends){
+            friend.style.backgroundColor = 'lightblue';
+        }
+});
+document.getElementById('add-friend').addEventListener('click', function(){
+    const container = document.getElementById('friend-container');
+    const friendDiv = document.createElement('div');
+    friendDiv.classList.add('friend');
+    friendDiv.innerHTML = `
+            <h1 class="friend-name">Friend-5</h1>
+            <p>Lorem, ipsum dolor.</p>
+            `;
+    container.appendChild(friendDiv);
+});
